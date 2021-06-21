af4920491c9756b6f1ad5fc3cb6d4698fc4bb6aa
Your Name <thangpdth2009031@fpt.edu.vn>
2021-06-21 07:37:57 +0700

    add lab 05 PhuongTrinhBacHai

---
+++ PhươngTrinhBacHai/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace PhươngTrinhBacHai
+{
+    internal class Program
+    {
+        public delegate double PhuongTrinhBacHai(int a, int b, int c);
+
+        public static double TinhToan(int a, int b, int c)
+        {
+            var Delta = Math.Pow(b, 2) - 4 * a * c;
+            if (Delta > 0)
+            {
+                Console.WriteLine("Phuong Trinh Co Hai Nghiem: ");
+                Console.WriteLine("x1 = {0}", (-b - Math.Sqrt(Delta)) / 2 * a);
+                Console.WriteLine("x2 = {0}", (-b + Math.Sqrt(Delta)) / 2 * a);
+            } else if (Delta == 0)
+            {
+                Console.WriteLine("Phuong trinh co hai nghiem kep: ");
+                Console.WriteLine("x1 = x2 = {0}", -b/2 * a);
+            }
+            else
+            {
+                Console.WriteLine("Phuong trinh vo nghiem");
+            }
+
+            return Delta;
+        }
+        public static void Main(string[] args)
+        {
+            var phuongTrinhBacHai = new PhuongTrinhBacHai(TinhToan);
+            var result = phuongTrinhBacHai(2, 6, 1);
+            Console.WriteLine("Delta = {0}", result);
+        }
+    }
+}
---
+++ lab06/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace lab06
+{
+    internal class Program
+    {
+        // public delegate void Increment(ref int x);
+        //
+        // public static void Add2(ref int x)
+        // {
+        //     x += 2;
+        // }
+        //
+        // public static void Add3(ref int x)
+        // {
+        //     x += 3;
+        // }
+        // public static void Main(string[] args)
+        // {
+        //     var increment = new Increment(Add2);
+        //     increment += Add3;
+        //     increment += Add2;
+        //     var x = 5;
+        //     increment(ref x);
+        //     Console.WriteLine(x);
+        // }
+        
+        
+        
+        //Tinh Tổng Ba Số
+        public delegate int ThangDelegate(int a, int b, int c);
+
+        public static int TinhTongBaSo(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        public static void Main(string[] args)
+        {
+            var thangDelegate = new ThangDelegate(TinhTongBaSo);
+            var result = thangDelegate(4, 2, 9);
+            Console.WriteLine(result);
+        }
+    }
+}
